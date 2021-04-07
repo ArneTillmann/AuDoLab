2b2d1af757f168f6668a8e2dcd367f1675327f4a
ArneTillmann <arne.tillmann.vellmar@gmail.com>
2021-04-07 18:17:07 +0200

    Bump version: 0.0.14 → 0.0.15

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ArneTillmann/AuDoLab',
-    version='0.0.14',
+    version='0.0.15',
     zip_safe=False,
 )
