6a5dcfa28146290f2fe5cbdb7fd5ec00fe9a05c3
ArneTillmann <arne.tillmann.vellmar@gmail.com>
2021-04-08 18:08:55 +0200

    change of the test file

--- tests/test_AuDoLab.py
+++ tests/test_AuDoLab.py
@@ -6,14 +6,14 @@ import pytest
 
 from click.testing import CliRunner
 
-# from load_data import data
-# from load_papers import papers
+from load_data import data
+from load_papers import papers
 import os
 import sys
 import numpy as np
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 from AuDoLab import cli
-# from AuDoLab import AuDoLab
+from AuDoLab import AuDoLab
 #
 
 
@@ -45,21 +45,17 @@ def test_command_line_interface():
     assert '--help  Show this message and exit.' in help_result.output
 
 
-#
-#
-#
-# audo = AuDoLab.AuDoLab()
+audo = AuDoLab.AuDoLab()
 # #papers2 = audo.scrape_abstracts("https://ieeexplore.ieee.org/search/searchresult.jsp?action=search&newsearch=true&matchBoolean=true&queryText=(%22Author%20Keywords%22:cotton)&highlight=true&returnFacets=ALL&returnType=SEARCH&matchPubs=true&rowsPerPage=100&pageNumber=1", "cotton")
-# papers_processed = audo.preprocessing(papers)
-# data_processed = audo.preprocessing(data)
-# data_tfidf_features, papers_tfidf_features = audo.tf_idf_features(
-#     data, papers, )
-# data_tfidf, papers_tfidf = audo.tf_idf(data, papers)
-# classifier = audo.one_class_svm(papers_tfidf, data_tfidf)
-# df_data = audo.choose_classifier(data_processed, classifier, 0)
-# if __name__ == '__main__':
-#     # lda = audo.lda_modeling(papers_processed)
-#     # audo.lda_visualize_topics()
-#
-#     lda2 = audo.lda_modeling(df_data, no_above=0.3)
-#     audo.lda_visualize_topics()
+papers_processed = audo.preprocessing(papers)
+data_processed = audo.preprocessing(data)
+data_tfidf_features, papers_tfidf_features = audo.tf_idf_features(
+    data, papers)
+data_tfidf, papers_tfidf = audo.tf_idf(data, papers)
+classifier = audo.one_class_svm(papers_tfidf, data_tfidf)
+df_data = audo.choose_classifier(data_processed, classifier, 0)
+if __name__ == '__main__':
+    # lda = audo.lda_modeling(papers_processed)
+    # audo.lda_visualize_topics()
+    lda2 = audo.lda_modeling(df_data, no_above=0.3)
+    audo.lda_visualize_topics()
