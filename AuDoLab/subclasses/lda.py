23e1dcc1b5324a1662584a4f6b4015a4e4260bda
AFThielmann <antonthielmann@t-online.de>
2021-04-09 10:42:41 +0200

    imports and commenting

--- AuDoLab/subclasses/lda.py
+++ AuDoLab/subclasses/lda.py
@@ -1,46 +1,46 @@
-import gensim
+
 import pyLDAvis.gensim
 import pyLDAvis
+from gensim import corpora, models
 
 
 class LDA:
     """
-    Keywordscraper
+    performs Latent Dirichlet Allocation Topic Modelling
     """
 
     def __init__(self):
         3+4
 
-    # def get_topics(lda_model, number_of_topics):
-    #     train_vecs = []
-    #     counter = 0
-    #
-    #     for i in range(len(df_processed["tokens"][df.index[df.iloc[:, 1] == 1].tolist()])):
-    #
-    #         # calculate the topic distribution for every tweet in test set
-    #         top_topics = lda_model.get_document_topics(
-    #             bow_corpus[i], minimum_probability=0.0
-    #         )
-    #         # get the distribution values for all topics
-    #         topic_vec = [top_topics[i][1] for i in range(number_of_topics)]
-    #         # include length of tweet as covariate, too
-    #         # topic_vec.extend([len(text['final_text'].iloc[i])])
-    #         train_vecs.append(topic_vec)
-    #         counter = counter + 1
-    #     return train_vecs
 
     @staticmethod
-    def preperation(df_processed, no_below, no_above):
+    def preperation(df_processed, no_below=None, no_above=None):
+        """Preprocessing for LDA
 
-        dictionary = gensim.corpora.Dictionary(df_processed["tokens"])
-        dictionary.filter_extremes(no_above=no_above, no_below=no_below)
+        Args:
+            df_processed ([DataFrame]): [DataFrame stat store tokenized text in col ["tokens"]]
+            no_below ([int]): [if word appears in less than no_below documents they are not considered]
+            no_above ([float]): [if e.g. 0.9, no words are taken into account that appear more often than 90%]
+
+        Returns:
+            [dictionary]: [returns the dictionary used for LDA]
+            [bow_corpus]: [Corpus used for LDA]
+        """
+
+        dictionary = corpora.Dictionary(df_processed["tokens"])
+        if no_below == None:
+            no_below = 0
+        if no_above == None:
+            no_above = 1
         bow_corpus = [dictionary.doc2bow(doc)
                       for doc in df_processed["tokens"]]
         return dictionary, bow_corpus
 
+#####---- Should not be necessary anymore ----############
+
     @staticmethod
     def preperation2(df_processed, no_above):
-        dictionary = gensim.corpora.Dictionary(df_processed["tokens"])
+        dictionary = corpora.Dictionary(df_processed["tokens"])
         dictionary.filter_extremes(no_above=no_above)
         bow_corpus = [dictionary.doc2bow(doc)
                       for doc in df_processed["tokens"]]
@@ -64,6 +64,21 @@ class LDA:
     @staticmethod
     def model(corpus, num_topics, id2word,
               random_state, passes):
+        """LDA model
+
+        Args:
+            corpus (iterable of list of (int, float), optional): [Stream of document vectors or sparse matrix of shape]
+            num_topics (int): [pre-defined number of topics]
+            id2word ({dict of (int, str): gensim.corpora.dictionary.Dictionary}) – 
+                                            Mapping from word IDs to words. It is 
+                                            used to determine the vocabulary size, 
+                                            as well as for debugging and topic printing.
+            random_state (int): [for recreating exact identical output]
+            passes (int): [Number of passes through the corpus during training.]
+
+        Returns:
+            [lda_model]: [returns lda_model output]
+        """
 
         lda_model = gensim.models.LdaMulticore(
             corpus,
@@ -77,6 +92,13 @@ class LDA:
 
     @staticmethod
     def visualize_topics(lda_model, bow_corpus, dictionary):
+        """Create pyLDAvis plots for LDA model output
+
+        Args:
+            lda_model ([type]): Output of lda_model
+            bow_corpus ([list of (int, int)]): BoW representation of document.
+            dictionary (gensim.corpora.dictionary.Dictionary): [Dict used for creating Corpus]
+        """
 
         visualization = pyLDAvis.gensim.prepare(
             lda_model, bow_corpus, dictionary, sort_topics=False
