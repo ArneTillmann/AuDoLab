4957dd995cf633f5d935d61d5111a937291b6d46
AFThielmann <antonthielmann@t-online.de>
2021-04-09 13:01:12 +0200

    commenting, imports and minor changes

--- AuDoLab/subclasses/preprocessing.py
+++ AuDoLab/subclasses/preprocessing.py
@@ -1,33 +1,35 @@
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-import re
+from re import compile as re_compile
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 
 
 class Preprocessor:
-    """
-    Keywordscraper
-    """
-
     def __init__(self):
-        4+5
+        4 + 5
 
-    def text_prepare(text):
+    def _text_prepare(text, language="english"):
+        """prepares text for lda and o-svm.
+        Removes stopwords, symbols, double spaces and numbers
 
-        REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
-        BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
-        NUMBERS = re.compile(r'\d+')
-        STOPWORDS = set(stopwords.words('english'))
+        Args:
+            text (numpy array): [column out of dataframe with documents]
+            language (string): [language for stopword removel, default: english]
+
+        Returns:
+            [numpy array]: [prepared documents]
+        """
+
+        REPLACE_BY_SPACE_RE = re_compile("[/(){}\[\]\|@,;]")
+        BAD_SYMBOLS_RE = re_compile("[^0-9a-z #+_]")
+        NUMBERS = re_compile(r"\d+")
+        STOPWORDS = set(stopwords.words(language))
 
         text = text.lower()
         # replace REPLACE_BY_SPACE_RE symbols by space in text
-        text = REPLACE_BY_SPACE_RE.sub('', text)
-        text = BAD_SYMBOLS_RE.sub('', text)
-        text = NUMBERS.sub('', text)
+        text = REPLACE_BY_SPACE_RE.sub("", text)
+        text = BAD_SYMBOLS_RE.sub("", text)
+        text = NUMBERS.sub("", text)
         # delete symbols which are in BAD_SYMBOLS_RE from text
         words = text.split()
         i = 0
@@ -36,32 +38,51 @@ class Preprocessor:
                 words.pop(i)
             else:
                 i += 1
-        text = ' '.join(map(str, words))  # delete stopwords from text
+        text = " ".join(map(str, words))  # delete stopwords from text
 
         return text
 
-    def lemmatize_text(text):
-        lemmatizer = WordNetLemmatizer()
-        return " ".join([lemmatizer.lemmatize(w, 'v') for w in text])
+    def _lemmatize_text(text):
+        """helper function
 
-    def basic_preprocessing(df):
+        Args:
+            text (numpy array): [dataframe column where documents are stored]
+
+        Returns:
+            [numpy array]: [lemmatized documents]
+        """
+        lemmatizer = WordNetLemmatizer()
+        return " ".join([lemmatizer.lemmatize(w, "v") for w in text])
+
+    def basic_preprocessing(df, column):
+        """calls helper functions from above and preprocesses documents
+        removes all stopwords and unnecessary terms, e.g. symbols, numbers, double spaces
+        and tokenizes documents
+
+        Args:
+            df (DataFrame): [dataframe where documents are stored in one column]
+            columns (String): [column name where documents are stored]
+
+        Returns:
+            [DataFrame]: [dataframe with preprocessed documents. 2 new columns are appended. The completely preprocessed
+            documents are stored in column ['tokens'], the only lemmatized documents are stored in columns ['lemma']]
+        """
         df_temp = df.copy(deep=True)
 
         try:
-            df_temp = df_temp.rename(
-                index=str, columns={'transcription': 'text'})
+            df_temp = df_temp.rename(index=str, columns={"transcription": "text"})
         except:
             pass
 
-        df_temp.loc[:, 'text'] = [Preprocessor.text_prepare(
-            x) for x in df_temp['text'].values]
-        df_temp["text"] = [
-            item for item in df_temp['text'] if not item.isdigit()]
+        df_temp.loc[:, column] = [
+            Preprocessor._text_prepare(x, "english") for x in df_temp[column].values
+        ]
+        df_temp[column] = [item for item in df_temp[column] if not item.isdigit()]
 
-        tokenizer = RegexpTokenizer(r'\w+')
+        tokenizer = RegexpTokenizer(r"\w+")
 
-        df_temp["tokens"] = df_temp["text"].apply(tokenizer.tokenize)
-        df_temp["lemma"] = df_temp["tokens"].apply(Preprocessor.lemmatize_text)
+        df_temp["tokens"] = df_temp[column].apply(tokenizer.tokenize)
+        df_temp["lemma"] = df_temp["tokens"].apply(Preprocessor._lemmatize_text)
         df_temp["tokens"] = df_temp["lemma"].apply(tokenizer.tokenize)
 
         return df_temp
--- AuDoLab/subclasses/tf_idf.py
+++ AuDoLab/subclasses/tf_idf.py
@@ -1,10 +1,5 @@
-
 from AuDoLab.subclasses.preprocessing import Preprocessor
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
 
 class Tf_idf:
@@ -13,10 +8,32 @@ class Tf_idf:
     """
 
     def __init__(self):
-        4+5
+        4 + 5
 
     @staticmethod
-    def tfidf_features(data, papers, ngrams=2, features=8000):
+    def tfidf(
+        data,
+        papers,
+        data_column="lemma",
+        papers_column="lemma",
+        ngrams=2,
+        features=8000,
+    ):
+        """[creates tf-idf objects for one-class SVM classification. The tf-idf scores are calculated over a joint corpus,
+        however, the target data and the out-of-domain training data are stored in seperate, as the one-class SVM is only trained on
+        the tf-idf scores of the out-of-domain training data]
+
+        Args:
+            data (DataFrame): [preprocessed target documents]
+            papers (DataFrame): [preprocessed out-of-domain training data]
+            data_colum (String): [name of columnin target dataframe where lemmatized documents are stored]. Defaults to 'lemma'
+            papers_colum (String): [name of column in out-of-domain training dataframe where lemmatized documents are stored]. Defaults to 'lemma'
+            ngrams (int, optional): [whether ngram are formed]. Defaults to 2.
+            features (int, optional): [number of max features]. Defaults to 8000.
+
+        Returns:
+            [data and papers]: [tfidf object data for target data and ou-of-domain training data]
+        """
 
         preprocessing = Preprocessor()
         df_temp = data.copy(deep=True)
@@ -26,38 +43,14 @@ class Tf_idf:
         papers_temp = Preprocessor.basic_preprocessing(papers_temp)
 
         tfidf_vectorizer = TfidfVectorizer(
-            ngram_range=(1, ngrams), max_features=features)
+            ngram_range=(1, ngrams), max_features=features
+        )
 
-        corpus = df_temp['lemma'].append(papers_temp['lemma'])
+        corpus = df_temp[data_column].append(papers_temp[papers_column])
         tfidf_vectorizer.fit(corpus)
 
-        data_corpus = df_temp["lemma"].tolist()
-        paper_corpus = papers_temp["lemma"].tolist()
-
-        data = tfidf_vectorizer.transform(data_corpus)
-        papers = tfidf_vectorizer.transform(paper_corpus)
-
-        return data, papers
-
-    @staticmethod
-    def tfidf(df, papers, ngrams=2, labels=True):
-
-        preprocessing = Preprocessor()
-        df_temp = df.copy(deep=True)
-        df_temp = Preprocessor.basic_preprocessing(df_temp)
-
-        papers_temp = papers.copy(deep=True)
-        papers_temp = Preprocessor.basic_preprocessing(papers_temp)
-
-        tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, ngrams))
-
-        corpus = df_temp['lemma'].append(papers_temp['lemma'])
-        tfidf_vectorizer.fit(corpus)
-
-        list_corpus = corpus.tolist()
-
-        data_corpus = df_temp["lemma"].tolist()
-        paper_corpus = papers_temp["lemma"].tolist()
+        data_corpus = df_temp[data_column].tolist()
+        paper_corpus = papers_temp[papers_column].tolist()
 
         data = tfidf_vectorizer.transform(data_corpus)
         papers = tfidf_vectorizer.transform(paper_corpus)
