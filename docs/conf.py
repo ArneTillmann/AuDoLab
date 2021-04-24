ef2a244e335beb963bc8de96ff7763bdf18ffbf8
ArneTillmann <36853006+ArneTillmann@users.noreply.github.com>
2021-04-24 14:02:02 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,12 +21,6 @@ import os
 import sys
 import sphinx_rtd_theme
 
-extensions = [
-    ...
-    'sphinx_rtd_theme'
-]
-
-
 
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -39,7 +33,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx_rtd_theme", ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
