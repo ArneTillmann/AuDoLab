bef3683b5650053dc799e465fc5431f261341bcc
ArneTillmann <arne.tillmann.vellmar@gmail.com>
2021-04-30 13:21:54 +0200

    test update

--- tests/test_AuDoLab.py
+++ tests/test_AuDoLab.py
@@ -1,12 +1,12 @@
-import pytest
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
-from AuDoLab import AuDoLab
-from load_papers import papers
-from load_data import data
-import numpy as np
-from IPython.core.display import HTML
+# import pytest
+# import os
+# import sys
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+# from AuDoLab import AuDoLab
+# from load_papers import papers
+# from load_data import data
+# import numpy as np
+# from IPython.core.display import HTML
 """Tests for `AuDoLab` package."""
 
 
@@ -26,42 +26,42 @@ def test_content(response):
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-audo = AuDoLab.AuDoLab()
-# papers = audo.scrape_abstracts(
-#     keywords=["dentistry", "teeth", "tooth"],
-#     in_data="all_meta",
-#     pages=12,
-#     operator="or"
-# )
-papers_processed = audo.preprocessing(papers, "text")
-data_processed = audo.preprocessing(data, "transcription")
-
-data_tfidf, papers_tfidf = audo.tf_idf( data_processed, papers_processed, data_column="lemma", papers_column="lemma")
-
-# nus = np.arange(0.001, 0.5, 0.001)
-# nus = np.round(nus, 5)
+# audo = AuDoLab.AuDoLab()
+# # papers = audo.scrape_abstracts(
+# #     keywords=["dentistry", "teeth", "tooth"],
+# #     in_data="all_meta",
+# #     pages=12,
+# #     operator="or"
+# # )
+# papers_processed = audo.preprocessing(papers, "text")
+# data_processed = audo.preprocessing(data, "transcription")
 #
-classifier = audo.one_class_svm(
-    training=papers_tfidf,
-    predicting=data_tfidf,
-    nus=[0.166,0.3],
-    quality_train=0.85,
-    min_pred=0.01,
-    max_pred=0.1,
-)
-
-df_data = audo.choose_classifier(data_processed, classifier, 0)
-
-if __name__ == '__main__':
-    # lda = audo.lda_modeling(papers_processed, num_topics=3,
-    # random_state=101,
-    # passes=10,
-    # no_below=2, no_above=0.9
-    # )
-
-    lda2 = audo.lda_modeling(df_data, no_above=0.3)
-
-    a = audo.lda_visualize_topics()
-    html = a.data
-    with open('html_file.html', 'w') as f:
-        f.write(html)
+# data_tfidf, papers_tfidf = audo.tf_idf( data_processed, papers_processed, data_column="lemma", papers_column="lemma")
+#
+# # nus = np.arange(0.001, 0.5, 0.001)
+# # nus = np.round(nus, 5)
+# #
+# classifier = audo.one_class_svm(
+#     training=papers_tfidf,
+#     predicting=data_tfidf,
+#     nus=[0.166,0.3],
+#     quality_train=0.85,
+#     min_pred=0.01,
+#     max_pred=0.1,
+# )
+#
+# df_data = audo.choose_classifier(data_processed, classifier, 0)
+#
+# if __name__ == '__main__':
+#     # lda = audo.lda_modeling(papers_processed, num_topics=3,
+#     # random_state=101,
+#     # passes=10,
+#     # no_below=2, no_above=0.9
+#     # )
+#
+#     lda2 = audo.lda_modeling(df_data, no_above=0.3)
+#
+#     a = audo.lda_visualize_topics()
+#     html = a.data
+#     with open('html_file.html', 'w') as f:
+#         f.write(html)
