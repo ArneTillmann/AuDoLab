ce9ef3cfddfadffff838635e5431a45573c632fa
Anton Thielmann <51477933+AFThielmann@users.noreply.github.com>
2021-06-04 08:36:40 +0200

    integrate new scraping

--- AuDoLab/AuDoLab.py
+++ AuDoLab/AuDoLab.py
@@ -1,18 +1,30 @@
 from numpy import round as np_round
 from numpy import arange as np_arange
+from pandas import concat as pd_concat
 from AuDoLab.subclasses import abstractscraper
 from AuDoLab.subclasses import lda
 from AuDoLab.subclasses import one_class_svm
 from AuDoLab.subclasses import preprocessing
 from AuDoLab.subclasses import tf_idf
+from AuDoLab.subclasses import abstractscraper_arxiv
+from AuDoLab.subclasses import abstractscraper_pubmed
 
 
 class AuDoLab:
     def __init__(self):
         4 + 4
 
-    async def scrape_abstracts(self, url=None, keywords=None, operator="OR", pages=2, in_data="author"):
-        """Function to scrap abstracts of scientific papers from the givin url.
+    async def scrape_abstracts(
+        self,
+        type="arxiv",
+        url=None,
+        keywords=None,
+        operator="OR",
+        pages=2,
+        in_data="author",
+        prepro=False
+    ):
+        """Function to scrape abstracts of scientific papers from the givin url.
         We used https://ieeexplore.ieee.org/search/advanced to generate a
         list like https://ieeexplore.ieee.org/search/searchresult.jsp?action=se
         arch&newsearch=true&matchBoolean=true&queryText=(%22Author%20Keywords%22
@@ -26,45 +38,40 @@ class AuDoLab:
         - url ( string)
         - file_name (string)
         """
+        if type == "ieee":
+            ks = abstractscraper.AbstractScraper()
+            self.abstracts = await ks.get_abstracts(
+                url, keywords, operator, pages, in_data
+            )
+            
 
-        ks = abstractscraper.AbstractScraper()
+        elif type == "arxiv":
+            ks = abstractscraper_arxiv.AbstractScraper_Arxiv()
+            self.abstracts = ks.scrape_arxiv(url, pages)
+            
 
-        self.abstracts = await ks.get_abstracts(url, keywords, operator, pages, in_data)
-        print(self.abstracts)
-        #file_name = file_name + ".txt"
-        #self.abstracts.to_csv(self.abstracts, header=True, index=False)
+        elif type == "pubmed":
+            ks = abstractscraper_pubmed.AbstractScraper_Pubmed()
+            self.abstracts = ks.scrape_pubmed(url, pages)
+
+        if prepro == True:
+            self.abstracts = self.text_cleaning(self.abstracts, "abstract")
+        
         return self.abstracts
 
-    def preprocessing(self, data, column):
-        """ The data will be lemmatized, tokenized and the stopwords will be
+    def text_cleaning(self, data, column):
+        """The data will be lemmatized, tokenized and the stopwords will be
         deleted.
 
         Arguments:
         - data (<class 'pandas.core.frame.DataFrame'>)
         """
         prepro = preprocessing.Preprocessor()
-        self.data_processed = prepro.basic_preprocessing(
-            data, column
-        )
+        self.data_processed = prepro.basic_preprocessing(data, column)
         return self.data_processed
 
-    # def tf_idf_features(self, data, papers, features=8000):
-    #     """The function tf_idf_features(...) calculates the tfidf scores, but
-    #     return only the <features> amount of words with the highest tfidf
-    #     scores.
-    #
-    #     Arguments:
-    #     - data (<class 'pandas.core.frame.DataFrame'>)
-    #     - papers (<class 'pandas.core.frame.DataFrame'>)
-    #     """
-    #     tfidf = tf_idf.Tf_idf()
-    #     self.data_tfidf_features, self.papers_tfidf_features =
-    #     tfidf.tfidf_features(
-    #         data, papers, features=features)
-    #     return self.data_tfidf_features, self.papers_tfidf_features
-
     def tf_idf(self, data, papers, data_column, papers_column, features=None):
-        """ The function tf_idf(...) calculates the tfidf scores.
+        """The function tf_idf(...) calculates the tfidf scores.
 
         Arguments:
         - data (<class 'pandas.core.frame.DataFrame'>)
@@ -126,11 +133,18 @@ class AuDoLab:
     def lda_modeling(
         self,
         data,
+        num_topics,
+        corpus=None,
+        dict=None,
         no_below=None,
         no_above=None,
-        num_topics=5,
         random_state=101,
         passes=20,
+        chunksize=500,
+        eta="auto",
+        eval_every=None,
+        multi=True,
+        alpha="asymmetric",
     ):
         """The function performs lda modelling as described in this
         https://www.jmlr.org/papers/volume3/blei03a/blei03a.pdf paper.
@@ -143,22 +157,72 @@ class AuDoLab:
         - random_state (int)
         - passes (int)
         """
+        if corpus == None:
+            self.dictionary, self.bow_corpus = lda.LDA.preperation(
+                data, no_below, no_above
+            )
 
-        self.dictionary, self.bow_corpus = lda.LDA.preperation(data, no_below,
-                                                               no_above)
+        else:
+            self.corpus = (corpus,)
+            self.dictionary = dict
+
         self.lda = lda.LDA()
         self.lda_model = self.lda.model(
-            self.bow_corpus,
+            corpus=self.bow_corpus,
             num_topics=num_topics,
             id2word=self.dictionary,
             random_state=random_state,
             passes=passes,
+            chunksize=chunksize,
+            eta=eta,
+            eval_every=eval_every,
+            multi=multi,
+            alpha=alpha,
         )
         return self.lda_model
 
-    def lda_visualize_topics(self):
+    def lda_visualize_topics(
+        self,
+        save_name="audolab_model.png",
+        lda_model=None,
+        bow_corpus=None,
+        dictionary=None,
+        type="pyldavis",
+        figsize=(20, 10),
+        facecolor="k",
+        width=1600,
+        height=800,
+        background_color="white",
+        topic=0,
+        words=100,
+        save=False,
+        all=False,
+    ):
         """The lda model calculated with the function lda_modeling is visualized
         in an html frame and opened in the standard browser.
         """
-        lda.LDA.visualize_topics(self.lda_model, self.bow_corpus,
-                                 self.dictionary)
+        if bow_corpus == None:
+            bow_corpus = self.bow_corpus
+
+        if dictionary == None:
+            dictionary = self.dictionary
+
+        if lda_model == None:
+            lda_model = self.lda_model
+
+        lda.LDA.visualize_topics(
+            lda_model,
+            bow_corpus=bow_corpus,
+            dictionary=dictionary,
+            save_name=save_name,
+            type=type,
+            figsize=figsize,
+            facecolor=facecolor,
+            width=width,
+            height=height,
+            background_color=background_color,
+            topic=topic,
+            words=words,
+            save=save,
+            all=all,
+        )
